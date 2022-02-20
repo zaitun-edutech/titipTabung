66fab1fe0026b1312e99f9fb854fb140660e1297
zaitunedutech@gmail.com <zaitunedutech@gmail.com>
2022-02-20 13:31:12 +0800

    menambahlkan total

--- src/Main.vue
+++ src/Main.vue
@@ -1,19 +1,29 @@
 <template>
-  <div class="container-fluid">
+  <div class="container">
     <div class="row justify-content-center">
       <div class="col-md-6 text-center">
-        <h1>Aplikasi Pencatatan Titip Tabung</h1>
-        <h2>Pangkalan Tabung Iffah Store</h2>
+        <div class="p-4 fw-bold">
+          <!-- <img src="./assets/top.png" alt="" /> -->
+          <h1>Aplikasi Pencatatan Titip Tabung</h1>
+          <h2>Pangkalan Tabung Iffah Store</h2>
+        </div>
       </div>
     </div>
 
     <div class="row justify-content-center">
-      <div class="col-md-6">
-        <button class="btn btn-primary mb-3" @click="postData()">
-          + Tambah Data
-        </button>
+      <div class="col-md-8">
+        <div>
+          <button class="btn btn-dark mb-3" v-if="post == true" @click="back()">
+            &laquo;Back
+          </button>
+        </div>
+        <div v-if="post == false">
+          <button class="btn btn-primary mb-3" @click="postData()">
+            + Tambah Data
+          </button>
+        </div>
         <div v-if="post == true">
-          <div class="card p-3 mb-4 rounded-3 bg-warning shadow-sm">
+          <div class="card p-3 mb-4 rounded-3 shadow-lg">
             <form @submit.prevent="addTitip" class="p-3 rounded-3">
               <div class="form-group">
                 <label for="nama" class="form-label">Nama</label>
@@ -83,6 +93,23 @@
                   <option value="tidak">Tidak</option>
                 </select>
               </div>
+              <div class="">
+                <label for="status" class="form-label"
+                  >Status Pengambilan</label
+                >
+                <select
+                  class="form-select"
+                  id="status"
+                  aria-label="Floating label select example"
+                  v-model="status"
+                  required
+                >
+                  <option selected value="">--pilih--</option>
+                  <option value="belum">Belum</option>
+                  <option value="sudah">Sudah</option>
+                </select>
+              </div>
+
               <div v-if="edit == false">
                 <button
                   class="btn btn-primary mt-3"
@@ -104,57 +131,79 @@
         </div>
 
         <!-- <div class="col-md-8"> -->
-
-        <table
-          id="example"
-          class="table table-hover table-responsive rounded shadow-sm"
-        >
-          <thead class="">
-            <tr class="text-center bg-primary text-white rounded">
-              <th>No</th>
-              <th>Nama</th>
-              <th>Tanggal</th>
-              <th>Jumlah Tabung</th>
-              <th>Jumlah Uang</th>
-              <th>Lunas Tabung</th>
-              <th>Lunas Uang</th>
-              <th v-if="post == false">Aksi</th>
-            </tr>
-          </thead>
-          <tbody>
-            <tr
-              v-for="(row, index) in titips"
-              :key="index"
-              class="fw-bold text-center"
-            >
-              <td>{{ index + 1 }}</td>
-              <td>{{ row.nama }}</td>
-              <td>{{ row.hari }}</td>
-              <td>{{ row.j_tabung }}</td>
-              <td>{{ row.j_uang }}</td>
-              <td>{{ row.l_tabung }}</td>
-              <td>{{ row.l_uang }}</td>
-              <td class="text-center" v-if="post == false">
-                <button class="badge bg-warning" @click="editTitip(row, index)">
-                  Edit
-                </button>
-                <button
-                  class="badge bg-danger ms-2"
-                  @click="deleteTitip(index)"
-                >
-                  delete
-                </button>
-              </td>
-            </tr>
-          </tbody>
-        </table>
+        <div v-if="post == false" class="table-responsive shadow">
+          <table id="example" class="table table-hover rounded align-middle">
+            <thead class="align-middle">
+              <tr class="text-center bg-primary text-white rounded">
+                <th>No</th>
+                <th>Nama</th>
+                <th>Tanggal</th>
+                <th>Jumlah Tabung</th>
+                <th>Jumlah Uang</th>
+                <th>Lunas Tabung</th>
+                <th>Lunas Uang</th>
+                <th>Status</th>
+                <th>Aksi</th>
+              </tr>
+            </thead>
+            <tbody>
+              <tr
+                v-for="(row, index) in titips"
+                :key="index"
+                class="fw-bold text-center"
+              >
+                <td>{{ index + 1 }}</td>
+                <td>{{ row.nama }}</td>
+                <td>{{ row.hari }}</td>
+                <td>{{ row.j_tabung }}</td>
+                <td>{{ row.j_uang }}</td>
+                <td>{{ row.l_tabung }}</td>
+                <td>{{ row.l_uang }}</td>
+                <td>{{ row.status }}</td>
+                <td class="text-center">
+                  <button
+                    class="badge bg-warning"
+                    @click="editTitip(row, index)"
+                  >
+                    Edit
+                  </button>
+                  <button
+                    class="badge bg-danger ms-2"
+                    @click="deleteTitip(index)"
+                  >
+                    delete
+                  </button>
+                </td>
+              </tr>
+              <tr class="text-center">
+                <td class="text-center">
+                  <h4>Total</h4>
+                </td>
+                <td></td>
+                <td></td>
+                <td>
+                  <h4>{{ totalTabung }}</h4>
+                  <hr />
+                </td>
+                <td>
+                  <h4>{{ totalUang }}</h4>
+                  <hr />
+                </td>
+                <td></td>
+                <td></td>
+                <td></td>
+                <td></td>
+              </tr>
+            </tbody>
+          </table>
+        </div>
       </div>
     </div>
   </div>
 </template>
 
 <script setup>
-import { onMounted, ref } from "vue";
+import { computed, onMounted, ref } from "vue";
 
 const nama = ref("");
 const hari = ref("");
@@ -162,6 +211,7 @@ const j_tabung = ref();
 const j_uang = ref();
 const l_tabung = ref("");
 const l_uang = ref("");
+const status = ref("");
 const titips = ref([]);
 const number = ref(1);
 const edit = ref(false);
@@ -176,6 +226,7 @@ function addTitip() {
     j_uang: j_uang.value,
     l_tabung: l_tabung.value,
     l_uang: l_uang.value,
+    status: status.value,
   };
   titips.value.push(newTitip);
   if (nama.value !== "") {
@@ -204,6 +255,7 @@ function editTitip(t, i) {
   j_uang.value = t.j_uang;
   l_tabung.value = t.l_tabung;
   l_uang.value = t.l_uang;
+  status.value = t.status;
   ind.value = i;
 }
 function updateTitip() {
@@ -216,6 +268,7 @@ function updateTitip() {
     j_uang: j_uang.value,
     l_tabung: l_tabung.value,
     l_uang: l_uang.value,
+    status: status.value,
   };
   titips.value[ind.value] = titipdb;
   localStorage.titips = JSON.stringify(titips.value);
@@ -241,11 +294,29 @@ function cancelTitip() {
   post.value = !post.value;
 }
 function deleteTitip(index) {
-  titips.value.splice(index, 1);
+  if (confirm("Are you sure")) {
+    titips.value.splice(index, 1);
+    localStorage.titips = JSON.stringify(titips.value);
+  }
 }
 function postData() {
   post.value = !post.value;
 }
+function back() {
+  post.value = !post.value;
+}
+const totalTabung = computed(() => {
+  return titips.value.reduce(
+    (total, curr) => (total = total + curr.j_tabung),
+    0
+  );
+});
+const totalUang = computed(() => {
+  return titips.value.reduce(
+    (total, hitung) => (total = total + hitung.j_uang),
+    0
+  );
+});
 </script>
 
 <style>
